309857ee62a950ed2725eb7833e80e96342cfde4
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-02-01 07:16:56 +0200

    update HomeController.cs - linked views

--- server/Controllers/HomeController.cs
+++ server/Controllers/HomeController.cs
@@ -18,6 +18,22 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    public IActionResult TermsAndConditions()
+    {
+        return View();
+    }
+
+
     public IActionResult Privacy()
     {
         return View();
