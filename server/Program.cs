72876e4897dca522d3706e595f2eb630ed514055
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-03-29 20:13:05 +0200

    feat: added Serilog as logging solution

--- server/Program.cs
+++ server/Program.cs
@@ -1,14 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using server.Data;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<serverContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("serverContext") ?? throw new InvalidOperationException("Connection string 'serverContext' not found.")));
+using Serilog;
+
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+try {
+    Log.Information("Starting web application");
+    Log.Debug("Lets Goooooooooooooooo!");
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddDbContext<serverContext>(options =>
+    options.UseSqlServer(builder.Configuration
+    .GetConnectionString("serverContext") ?? throw new InvalidOperationException("Connection string 'serverContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
+builder.Services.AddCors();
+// logger
+builder.Host.UseSerilog();
 
-var app = builder.Build();
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -21,12 +34,20 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
-
-app.UseAuthorization();
+app.UseRouting();;
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
