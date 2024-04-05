92b35a9cbbbbc6e419ac312d7354b10930a4dd41
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-04-05 04:48:16 +0200

    update Program.cs, added `auth` & logging

--- server/Program.cs
+++ server/Program.cs
@@ -1,47 +1,65 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using server.Data;
 using Serilog;
+using FlexibleJobs.Data;
 
-Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
 
-try {
+try
+{
     Log.Information("Starting web application");
-    Log.Debug("Lets Goooooooooooooooo!");
+    Log.Debug("[DEBUG]Lets Goooooooooooooooo!"); //TODO: check how to use debug level logs and show in console
+    Log.Warning("This is very sus!");
+    Log.Verbose("This is quite verbose, young Sir"); //TODO: check on using verbose logging
     var builder = WebApplication.CreateBuilder(args);
-    builder.Services.AddDbContext<serverContext>(options =>
-    options.UseSqlServer(builder.Configuration
-    .GetConnectionString("serverContext") ?? throw new InvalidOperationException("Connection string 'serverContext' not found.")));
+    builder.Services.AddDbContext<FlexibleJobsContext>(options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("FlexibleJobsContext")
+                ?? throw new InvalidOperationException(
+                    "Connection string 'FlexibleJobsContext' not found."
+                )
+        )
+    );
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication();
-builder.Services.AddCors();
-// logger
-builder.Host.UseSerilog();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    builder
+        .Services.AddAuthentication()
+        .AddCookie(
+            /** https://learn.microsoft.com/en-us/aspnet/core/security/authentication/?view=aspnetcore-8.0 */
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            options => builder.Configuration.Bind("CookieSettings", options)
+        );
+    builder.Services.AddAuthorization();
+    builder.Services.AddCors();
+
+    // logger
+    builder.Host.UseSerilog();
 
     var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    app.UseSerilogRequestLogging();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-app.UseRouting();;
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseCors();
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
-
+    app.Run();
 }
 catch (Exception ex)
 {
