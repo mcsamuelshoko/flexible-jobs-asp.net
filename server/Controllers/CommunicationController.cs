9f657b723ff910fd14ad7c6616494ef147d2ddae
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-04-05 04:57:55 +0200

    feat: create CommunicationController.cs

---
+++ server/Controllers/CommunicationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlexibleJobs.Controllers
+{
+    public class CommunicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
